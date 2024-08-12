97807ead18e854107c028ece866c524144b578b8
Zachary Atkins <atkins12@llnl.gov>
2024-08-11 19:10:46 -0600

    70 seconds

---
+++ graveler.jl
@@ -0,0 +1,38 @@
+using Distributed
+@everywhere using Distributed
+@everywhere using ProgressBars
+@everywhere using Random
+
+
+@everywhere roll(rng, a) = sum(rand!(rng, a, 1:4) .== 1)
+
+@everywhere function roll_chunk(chunk_size::Int; target::Int=177)
+  # Initialize RNG and array to store values
+  # This is faster than using rand without an RNG, since we don't have to recreate it each time
+  rng = TaskLocalRNG()
+  a = zeros(UInt8, 231)
+  max_ones = 0
+  num_rolls = 0
+  while max_ones < target && num_rolls <= chunk_size
+    max_ones = max(max_ones, roll(rng, a))
+    num_rolls += 1
+  end
+  return max_ones, num_rolls
+end
+
+@everywhere reduce_chunk_sums(lhs, rhs) = max(first(lhs), first(rhs)), last(lhs) + last(rhs)
+
+function main()
+  num_procs = 24
+  n = Int(1e9)
+  chunk_size = n ÷ num_procs
+  values = Distributed.pmap(roll_chunk, fill(chunk_size, num_procs))
+  max_ones, num_rolls = Iterators.reduce(reduce_chunk_sums, values)
+  println("Max ones: $max_ones")
+  println("Num rolls: $num_rolls")
+end
+
+let
+  addprocs(24)
+  @time main()
+end
--- graveler.py
+++ graveler.py
@@ -1,20 +1,27 @@
-import random
-import math
-from itertools import repeat
+from numpy.random import Generator, PCG64
+from tqdm.contrib.concurrent import thread_map, process_map  # or thread_map
 
-items = [1,2,3,4]
-numbers = [0,0,0,0]
-rolls = 0
-maxOnes = 0
 
-while numbers[0] < 177 and rolls < 1000000000:
-    numbers = [0,0,0,0]
-    for i in repeat(None, 231):
-        roll = random.choice(items)
-        numbers[roll-1] = numbers[roll-1] + 1
-    rolls = rolls + 1
-    if numbers[0] > maxOnes:
-        maxOnes = numbers[0]
+def roll_n(n):
+    max_ones = 0
+    rng = Generator(PCG64())
 
-print("Highest Ones Roll:",maxOnes)
-print("Number of Roll Sessions: ",rolls)
+    for roll in range(1, n + 1):
+        num_ones = sum(rng.integers(1, 5, size=231) == 1)
+        max_ones = max(num_ones, max_ones)
+        if max_ones >= 177:
+            break
+    return max_ones, roll
+
+
+if __name__ == "__main__":
+    # chuck size takes about 10 seconds to run
+    chunk_size = int(100_000)
+    max_rolls = int(1_000_000_000)
+    result = process_map(roll_n, [chunk_size] * (max_rolls // chunk_size), chunksize=10)
+    max_ones_list, num_rolls_list = zip(*result)
+    max_ones = max(max_ones_list)
+    num_rolls = sum(num_rolls_list)
+
+    print("Highest Ones Roll:", max_ones)
+    print("Number of Roll Sessions: ", num_rolls)
