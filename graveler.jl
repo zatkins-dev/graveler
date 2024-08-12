9efd6b204a9b2d6027a90a31ae561d1415e3719d
Zachary Atkins <atkins12@llnl.gov>
2024-08-11 19:34:06 -0600

    fix first run behavior

--- graveler.jl
+++ graveler.jl
@@ -1,4 +1,7 @@
 using Distributed
+num_procs = 24
+addprocs(num_procs)
+
 @everywhere using Distributed
 @everywhere using ProgressBars
 @everywhere using Random
@@ -13,7 +16,7 @@ using Distributed
   a = zeros(UInt8, 231)
   max_ones = 0
   num_rolls = 0
-  while max_ones < target && num_rolls <= chunk_size
+  while max_ones < target && num_rolls < chunk_size
     max_ones = max(max_ones, roll(rng, a))
     num_rolls += 1
   end
@@ -22,17 +25,11 @@ end
 
 @everywhere reduce_chunk_sums(lhs, rhs) = max(first(lhs), first(rhs)), last(lhs) + last(rhs)
 
-function main()
-  num_procs = 24
+let
   n = Int(1e9)
   chunk_size = n ÷ num_procs
-  values = Distributed.pmap(roll_chunk, fill(chunk_size, num_procs))
+  @time values = Distributed.pmap(roll_chunk, fill(chunk_size, num_procs))
   max_ones, num_rolls = Iterators.reduce(reduce_chunk_sums, values)
   println("Max ones: $max_ones")
   println("Num rolls: $num_rolls")
-end
-
-let
-  addprocs(24)
-  @time main()
 end
